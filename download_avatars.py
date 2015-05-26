a59a18c2e5f5ede7907ee69f725af5df22ceb555
Michael Käufl <opendata-stuttgart@c.michael-kaeufl.de>
2015-05-26 22:20:57 +0200

    Search for lab members in _labs/*.yml instead of */index.html

--- download_avatars.py
+++ download_avatars.py
@@ -30,12 +30,15 @@ AVATAR_PATH = 'img/avatars/'
 def main():
     existing = glob.glob(AVATAR_PATH + '*.jpg')
     existing = [x.split('/')[-1].split('.')[0] for x in existing]
-    labs = [x for x in glob.glob('*/index.html') if not x.startswith(BLACKLIST)]
+    labs = [x for x in glob.glob('_labs/*.yml') if not x.startswith(BLACKLIST)]
     for lab in labs:
         logging.debug('Processing Lab %s', lab)
         with open(lab) as f:
             contents = f.read()
-            _, frontmatter, _ = contents.split('---\n', 2)
+            try:
+                _, frontmatter = contents.split('---\n', 2)
+            except ValueError:
+                _, frontmatter, _ = contents.split('---\n', 2)
             meta = yaml.load(frontmatter)
             if 'members' not in meta:
                 continue
