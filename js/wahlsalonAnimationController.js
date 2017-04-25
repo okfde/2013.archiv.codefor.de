e227f9a90ad17a01ca3d156b6aa4d8d6be328ec8
Alex Tolar <alextolar@hort.local>
2017-04-25 15:55:34 +0200

    chrome retina background svg fix

--- js/wahlsalonAnimationController.js
+++ js/wahlsalonAnimationController.js
@@ -12,6 +12,11 @@
 		$(window).bind('scroll',function(e){
 		    ref.parallaxScroll();
 		});
+
+
+    this.browser = this.getBrowser();
+    $('html').addClass(ref.browser.name.toLowerCase()).addClass('version-' + ref.browser.version.toLowerCase());
+
 	};
 
 	wsac.prototype.parallaxScroll = function() {
@@ -33,6 +38,25 @@
     $('.ws-page-wrap').css('background-position-y',positionCss);
 
 	};
+
+  wsac.prototype.getBrowser = function()
+  {
+      var ua=navigator.userAgent,tem,M=ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+      if(/trident/i.test(M[1])){
+          tem=/\brv[ :]+(\d+)/g.exec(ua) || [];
+          return {name:'IE',version:(tem[1]||'')};
+      }
+      if(M[1]==='Chrome'){
+          tem=ua.match(/\bOPR\/(\d+)/)
+          if(tem!=null)   {return {name:'Opera', version:tem[1]};}
+      }
+      M=M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+      if((tem=ua.match(/version\/(\d+)/i))!=null) {M.splice(1,1,tem[1]);}
+      return {
+          name: M[0],
+          version: M[1]
+      };
+  };
 
   window.wsac = wsac;
 
