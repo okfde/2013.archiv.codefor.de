247e7f8199ff560404db54ef0907e9ef84c6fc1c
konstin <konstin@mailbox.org>
2018-12-15 12:31:15 +0100

    Update avatars

--- update_avatars.py
+++ update_avatars.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """
 This script updates all the avatars for the lab members.
 
-If you want to re-download all images, you need to create a personal access token on github and add to both http
-requests using basic auth. Otherwise you'll be stopped half-way by github's rate limits.
+If you want to re-download all images, you need to create a personal access token on github and
+set it through `GITHUB_AUTH_TOKEN`. Otherwise you'll be stopped halfway by github's rate limits.
 """
 import glob
 import os
@@ -26,7 +26,12 @@ def get_avatar(username, last_modified):
     :param last_modified: timestamp formatted in the http stlye
     :return: True if the profile didn't change didn't change, False if there was an error or the avatar
     """
-    response = requests.get('https://api.github.com/users/' + username, headers={'If-Modified-Since': last_modified})
+    headers = {'If-Modified-Since': last_modified}
+    github_auth_token = os.environ.get("GITHUB_AUTH_TOKEN", None)
+    if github_auth_token:
+        headers["Authorization"] = "token " + github_auth_token
+
+    response = requests.get('https://api.github.com/users/' + username, headers=headers)
     if response.status_code == 304:
         return False
 
@@ -35,7 +40,7 @@ def get_avatar(username, last_modified):
         return False
 
     url = response.json()['avatar_url']
-    avatar = requests.get(url, headers={'If-Modified-Since': last_modified})
+    avatar = requests.get(url, headers=headers)
     if response.status_code == 304:
         return False
 
@@ -51,6 +56,7 @@ def main():
         last_modified = fd.readline().rstrip()
     current_timestamp_formatted = formatdate(timeval=None, localtime=False, usegmt=True)
 
+    usernames = set()
     labs = [x for x in glob.glob('_labs/*.yml') if not x.startswith(BLACKLIST)]
     for lab in labs:
         with open(lab) as f:
@@ -68,21 +74,27 @@ def main():
             if member['name'] is None:
                 continue
 
-            logging.info('Processing Lab Member %s', member['name'])
-
-            if 'username-github' not in member:
+            username = member.get('username-github')
+            if username == None:
                 continue
 
-            username = member['username-github']
-            if username is not None:
-                avatar = get_avatar(username, last_modified)
-            if not avatar:
-                continue
+            usernames.add((username, member['name']))
 
-            avatar_path = os.path.join(AVATAR_PATH, username + '.jpg')
-            logging.info('Saving image to %s', avatar_path)
-            with open(avatar_path, 'wb') as fd:
-                fd.write(avatar)
+    usernames = sorted(list(usernames))
+    logging.info("Found %d users", len(usernames))
+
+    for username, member_name in usernames:
+        logging.info('Processing Lab Member %s (%s)', member_name, username)
+
+        avatar = get_avatar(username, last_modified)
+        if not avatar:
+            continue
+
+        avatar_path = os.path.join(AVATAR_PATH, username + '.jpg')
+        print(avatar_path, AVATAR_PATH, username)
+        logging.info('Saving image to %s', avatar_path)
+        with open(avatar_path, 'wb') as fd:
+            fd.write(avatar)
 
     # Remember the last successful run for the If-Last-Modified header
     with open(os.path.join(AVATAR_PATH, 'last_modified.txt'), 'w') as fd:
